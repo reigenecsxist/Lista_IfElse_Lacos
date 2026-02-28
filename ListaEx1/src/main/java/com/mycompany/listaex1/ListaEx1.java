0e503aeba7a6001fd8ed0df7abc16723269c55f1
reigenecsxist <gabrielbiel.mesquita7@gmail.com>
2026-02-28 00:31:23 -0300

    Comitando

--- ListaEx1/src/main/java/com/mycompany/listaex1/ListaEx1.java
+++ ListaEx1/src/main/java/com/mycompany/listaex1/ListaEx1.java
@@ -1,11 +1,14 @@
-
 package com.mycompany.listaex1;
-
 import javax.swing.JOptionPane;
 
 public class ListaEx1 {
-
+    static class Hello{
+        Hello(){
+            JOptionPane.showMessageDialog(null, "Hello World!", "Hello", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello World");
+        Hello hl = new Hello();
     }
 }
