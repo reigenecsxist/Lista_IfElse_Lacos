0e2b1f6af5174819ebcb431d79007759f404a469
reigenecsxist <gabrielbiel.mesquita7@gmail.com>
2026-03-25 21:28:53 -0300

    Fiz um pouco dessa porra

--- Lista_IfElse_Lacos/src/main/java/com/mycompany/lista_ifelse_lacos/Exercicio5.java
+++ Lista_IfElse_Lacos/src/main/java/com/mycompany/lista_ifelse_lacos/Exercicio5.java
@@ -9,5 +9,21 @@ import javax.swing.JOptionPane;
  * @author Usuário do Windows
  */
 public class Exercicio5 {
+
+    Exercicio5() {
+        int n1 = Integer.parseInt(JOptionPane.showInputDialog("Insira o primeiro número:"));
+        int n2 = Integer.parseInt(JOptionPane.showInputDialog("Insira o segundo número:"));
+        int n3 = Integer.parseInt(JOptionPane.showInputDialog("Insira o terceiro número:"));
+        
+        int swap;
+        
+        if(n1>n2||n1>n3){
+            swap = n1;
+            if(n2>n3) JOptionPane.showMessageDialog(null, "A ordem crescente dos números é: "+n1+", "+n2+", "+n3+".");
+        }
+    }
     
+    public static void main(String[] args) {
+        Exercicio5 ex5 = new Exercicio5();
+    }
 }
